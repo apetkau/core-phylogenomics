d4e07571e2ff092c0e0ab1ffa606a1ecd0499feb
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2011-10-26 19:02:10 +0000

    Fixed up reporting so it takes into account badaligns removed from pipeline

--- lib/report.pl
+++ lib/report.pl
@@ -8,31 +8,54 @@ use Getopt::Long;
 use Bio::Seq;
 use Bio::SeqIO;
 
+my $verbose = 0;
+
 __PACKAGE__->run() unless caller;
 
 1;
 
 sub usage
 {
-	print "Usage: $0 -c <core_dir> -f <fasta_input_dir> [-o <output-file>\n";
+	print "Usage: $0 -c <core_dir> -a <align_dir> -f <fasta_input_dir> [-o <output-file> -v]\n";
+	print "\t-c|--core-dir:  The job data core directory\n";
+	print "\t-a|--align-dir:  The job data align directory\n";
+	print "\t-f|--fasta-input-dir:  The job data fasta input directory\n";
+	print "\t-o|--output-file:  The output file (optional)\n";
+	print "\t-v|--verbose:  Verbosity (optional)\n";
 }
 
 sub report_core_locus
 {
-	my ($core_dir) = @_;
+	my ($core_dir,$align_dir) = @_;
 	my %total_locus_lengths;
 	my $core_locus_count = 0;
 
-	# loci count
-	opendir(my $core_dh, $core_dir) or die "Could not open directory $core_dir: $!";
-	my @files = readdir($core_dh);
-	closedir($core_dh);
+	# gets snps/core genes used in pipeline (assumes all files under align/ directory are used)
+	opendir(my $align_dh, $align_dir) or die "Could not open directory $align_dir: $!";
+	my @align_files = grep {/^snps\d+\.aln$/} readdir($align_dh);
+	closedir($align_dh);
 
-	for my $file (@files)
+	# loci count
+	for my $align_file (@align_files)
 	{
-		if ($file =~ /^snps\d+$/)
+		if ($align_file =~ /^(snps\d+)\.aln$/)
 		{
-			my $full_file_path = "$core_dir/$file";
+			my $core_file = $1;
+			if (not defined $core_file)
+			{
+				print STDERR "Warning: core_file for $align_dir/$align_file not defined, skipping...";
+				next;
+			}
+
+			my $full_file_path = "$core_dir/$core_file";
+
+			if (not -e $full_file_path)
+			{
+				print STDERR "Warning: core_file=$full_file_path for $align_dir/$align_file does not exist, skipping...";
+				next;
+			}
+
+			print STDERR "processing $full_file_path\n" if ($verbose);
 			my $in = new Bio::SeqIO(-file=>"$full_file_path", -format=>"fasta");
 			my  @orfs;
 			while (my $seq = $in->next_seq)
@@ -74,6 +97,7 @@ sub report_initial_strains
 
 			my $strain_id = undef;
 			my $full_file_path = "$fasta_dir/$file";
+			print STDERR "processing $full_file_path\n" if ($verbose);
 			my $in = new Bio::SeqIO(-file=>"$full_file_path", -format=>"fasta");
 			while (my $seq = $in->next_seq)
 			{
@@ -107,21 +131,27 @@ sub report_initial_strains
 
 sub run
 {
-	my ($core_dir,$fasta_dir,$output_file);
+	my ($core_dir,$align_dir,$fasta_dir,$output_file);
 
 	if ( @_ && $_[0] eq __PACKAGE__)
 	{
 		GetOptions('c|core-dir=s' => \$core_dir,
+			'a|align-dir=s' => \$align_dir,
 			'o|output-file=s' => \$output_file,
-			'f|fasta-input-dir=s' => \$fasta_dir) or die "Invalid options\n".usage;
+			'f|fasta-input-dir=s' => \$fasta_dir,
+			'v|verbose' => \$verbose) or die "Invalid options\n".usage;
 	}
 	else
 	{
-		($core_dir,$fasta_dir,$output_file) = @_;
+		($core_dir,$align_dir,$fasta_dir,$output_file,$verbose) = @_;
 	}
+
+	$verbose = 0 if (not defined $verbose);
 
 	die "core-dir not defined\n".usage if (not defined $core_dir);
 	die "core-dir=$core_dir not a valid directory\n".usage if (not -d $core_dir);
+	die "align-dir not defined\n".usage if (not defined $align_dir);
+	die "align-dir=$align_dir not a valid directory\n".usage if (not -d $align_dir);
 	die "fasta-dir not defined\n".usage if (not defined $fasta_dir);
 	die "fasta-dir=$fasta_dir not a valid directory\n".usage if (not -d $fasta_dir);
 
@@ -135,7 +165,7 @@ sub run
 		open($output_fh, '>-') or die "Could not open stdout for writing";
 	}
 
-	my ($core_locus_count,$total_core_lengths) = report_core_locus($core_dir);
+	my ($core_locus_count,$total_core_lengths) = report_core_locus($core_dir,$align_dir);
 	my ($total_strain_loci,$total_features_lengths) = report_initial_strains($fasta_dir);
 
 	foreach my $strain (sort keys %$total_strain_loci)
@@ -143,8 +173,8 @@ sub run
 		my $curr_total_loci = $total_strain_loci->{$strain};
 		my $curr_total_length = $total_features_lengths->{$strain};
 		my $curr_core_lengths = $total_core_lengths->{$strain};
-		my $percent_loci = sprintf "%02.2f",($core_locus_count/$curr_total_loci);
-		my $percent_sequence = sprintf "%02.2f",($curr_core_lengths/$curr_total_length);
+		my $percent_loci = sprintf "%02.3f",($core_locus_count/$curr_total_loci);
+		my $percent_sequence = sprintf "%02.3f",($curr_core_lengths/$curr_total_length);
 
 		print "$strain: compared $percent_loci% ($core_locus_count/$curr_total_loci) of loci, $percent_sequence% ($curr_core_lengths/$curr_total_length) of sequence\n";
 	}
