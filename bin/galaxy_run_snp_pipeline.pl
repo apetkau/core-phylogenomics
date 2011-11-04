d9e8fbd322b2671e0643b5602bd91bd3530c3b90
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2011-11-04 17:37:15 +0000

    Added ability to define files in galaxy

--- bin/galaxy_run_snp_pipeline.pl
+++ bin/galaxy_run_snp_pipeline.pl
@@ -5,7 +5,8 @@ use warnings;
 
 use FindBin;
 
-$ENV{'PERL5LIB'} = "/opt/rocks/lib/perl5/site_perl/5.10.1:".$ENV{'PERL5LIB'};
+$ENV{'PERL5LIB'} = "/opt/rocks/lib/perl5/site_perl/5.10.1"
+    .((defined $ENV{'PERL5LIB'}) ? ':'.$ENV{'PERL5LIB'} : '');
 
 use lib $FindBin::Bin.'/../lib';
 
@@ -31,11 +32,10 @@ my $processors_opt;
 my $input_fasta_opt;
 my $help_opt;
 my $strain_count_opt;
-my $input_dir_opt;
-my $keep_files_opt;
 my $output_opt;
 my $pid_cutoff_opt;
 my $hsp_length_opt;
+my $input_files;
 
 my $resubmit_opt;
 my $start_stage_opt;
@@ -44,9 +44,11 @@ my $tmp_dir_opt;
 
 if (!GetOptions(
     'p|pid-cutoff=f' => \$pid_cutoff_opt,
+    'i|input-file=s@' => \$input_files,
     'h|hsp-length=i' => \$hsp_length_opt,
     'o|output=s' => \$output_opt,
-    't|tmp-dir=s' => \$tmp_dir_opt))
+    't|tmp-dir=s' => \$tmp_dir_opt,
+    'processors=i' => $processors_opt))
 {
     die "$!";
 }
@@ -56,17 +58,33 @@ die "Error: invalid tmp-dir=$tmp_dir_opt" if (not -d $tmp_dir_opt);
 die "Error: invalid output" if (not defined $output_opt);
 die "Error: invalid pid-cutoff" if (defined $pid_cutoff_opt and $pid_cutoff_opt !~ /\d+\.?\d*/);
 die "Error: invalid hsp-length" if (defined $hsp_length_opt and $hsp_length_opt !~ /\d+/);
+die "Error: invalid processors" if ((not defined $processors_opt) or $processors_opt !~ /\d+/);
+die "Error: no input files" if (not defined $input_files or @$input_files <= 0);
+foreach my $file (@$input_files)
+{
+    die "Error: input file is not defined" if (not defined $file);
+    die "Error: input file=$file does not exist" if (not -e $file);
+}
 my $output_dir = $output_opt;
 my $tmp_dir = $tmp_dir_opt;
 
 my $tmp_output_dir = tempdir('core_snp.XXXXXX', DIR => $tmp_dir);
+my $input_dir = "$tmp_output_dir/galaxy.input";
+mkdir($input_dir) or die "Could not make directory $input_dir: $!";
+
+foreach my $file (@$input_files)
+{
+    my $file_name = basename($file);
+    copy($file,"$input_dir/$file_name.fasta") or die "Could not copy $file to $input_dir: $!";
+}
 
 my $pseudoalign_out_file = "$tmp_output_dir/pseudoalign/pseudoalign.phy";
 my $pseudoalign_done_file = "$tmp_output_dir/stages/pseudoalign.done";
 
-my $pipeline_control_command = "$script_dir/snp_phylogenomics_control.pl --output \"$tmp_output_dir\" --input-dir \"$script_dir/../sample\" --processors 8 -v -v -v --force-output-dir";
+my $pipeline_control_command = "$script_dir/snp_phylogenomics_control.pl --output \"$tmp_output_dir\" --processors $processors_opt -v -v -v --force-output-dir --input-dir \"$input_dir\"";
 $pipeline_control_command .= " --pid-cutoff $pid_cutoff_opt" if (defined $pid_cutoff_opt);
 $pipeline_control_command .= " --hsp-length $hsp_length_opt" if (defined $hsp_length_opt);
+print "Executing: $pipeline_control_command\n";
 system("$pipeline_control_command") == 0 or die "Error in command $pipeline_control_command";
 if (-e $pseudoalign_done_file)
 {
