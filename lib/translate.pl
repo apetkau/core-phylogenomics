48214ed1fcc284cc92a6c25620a33da7b67e1be4
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2012-01-24 20:06:35 +0000

    Added better translate script

--- lib/translate.pl
+++ lib/translate.pl
@@ -1,12 +1,28 @@
 #!/usr/bin/perl
 use strict;
 use Bio::SeqIO;
-my $x=1;
-opendir (DH, ".");
-my @files = grep { /snps\d+$/i } readdir(DH);
-for my $file (@files) {
-my $in = new Bio::SeqIO(-file=>$file,-format=>"fasta");
-my $out = new Bio::SeqIO(-file=>">$file" . ".faa", -format=>"fasta");
-while (my $seq=$in->next_seq) {$out->write_seq($seq->translate);
+use File::Basename qw(basename);
+
+sub usage
+{
+	"Usage: ".basename($0)." <input dir> <output dir>\n";
 }
+
+my $input_dir = $ARGV[1];
+my $output_dir = $ARGV[2];
+
+
+die "No input dir defined\n".usage if (not defined $input_dir);
+die "Input dir does not exist\n".usage if (not -e $input_dir);
+
+die "No output dir defined\n".usage if (not defined $output_dir);
+die "Output dir exists\n".usage if (-e $output_dir);
+
+opendir (DH, $input_dir) or die "Could not open $input_dir: $!";
+my @files = grep { /fasta$/i } readdir(DH);
+for my $file (@files)
+{
+	my $in = new Bio::SeqIO(-file=>"$input_dir/$file",-format=>"fasta");
+	my $out = new Bio::SeqIO(-file=>">$output_dir/$file" . ".fasta", -format=>"fasta");
+	while (my $seq=$in->next_seq) {$out->write_seq($seq->translate);}
 }
