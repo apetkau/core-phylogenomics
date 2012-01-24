9f3715a75c7e297ab60695cf8ee2d1e8dce6f6ac
Aaron Petkau <aaron.petkau@phac-aspc.gc.ca>
2012-01-24 19:49:23 +0000

    More work on filtering out orthomcl orthologous clusters

--- lib/alignments_orthomcl.pl
+++ lib/alignments_orthomcl.pl
@@ -18,38 +18,107 @@ __PACKAGE__->run() unless caller;
 sub usage
 {
 	return
-"Usage: ".basename($0).".pl -i <input_dir> -f <fasta_input> -o <output_dir>\n";
+"Usage: ".basename($0).".pl -i <input_dir> -f <fasta_input> -o <output_dir> --strain-id <strain id> ...\n";
 }
 
-sub run
+sub create_set_table
 {
-	my ($orthomcl_input,$fasta_input,$output_dir);
-
-	if ( @_ && $_[0] eq __PACKAGE__)
+	my ($set_params) = @_;
+	my %ortho_group;
+	my $ortho_group_string = '';
+	my $first = 1;
+	foreach my $i (@$set_params)
 	{
-		GetOptions('i|orthomcl-input=s' => \$orthomcl_input,
-		   'f|fasta-input=s' => \$fasta_input,
-		   'o|output-dir=s' => \$output_dir) or die "Invalid options\n".usage;
+		$ortho_group{$i} = 1;
+
+		if ($first)
+		{
+			$ortho_group_string .= $i;
+			$first = 0;
+		}
+		else
+		{
+			$ortho_group_string .= ", $i";
+		}
 	}
-	else
+
+	return (\%ortho_group, $ortho_group_string);
+}
+
+# Input:  ortho_input  The orthomcl input groups file
+# Output:  A list containing all the good groups which should be processed
+sub handle_ortho_groups
+{
+	my ($ortho_input,$ortho_group) = @_;
+
+	my @ortho_good_group;
+
+	my $number_valid_strains_in_set = scalar(keys %$ortho_group);
+
+	open(my $g_h, "<$ortho_input") or die "Could not open groups file $ortho_input: $!";
+	my $line = readline($g_h);
+
+	my $group_kept = 0;
+	my $group_filtered = 0;
+	while (defined $line)
 	{
-		($orthomcl_input, $fasta_input, $output_dir) = @_;
+		my @group_en = split(/\s+/, $line);
+		my %existence_hash;
+		my @good_ortho_values; # stores the combined strain/locus tag names for the current group
+	
+		my ($group_name) = ($group_en[0] =~ /^([^:]*)/);
+		die "Error: invalid group name ".(defined $group_name ? $group_name : 'undefined') if (not defined $group_name);
+
+		for(my $g = 1; $g < scalar(@group_en); $g++)
+		{
+			my ($strain,$locus_id) = split(/\|/, $group_en[$g]);
+			die "No strain name found for $group_en[$g] on $line" if ((not defined $strain) or $strain eq '');
+			die "No locus_id found for $group_en[$g] on $line" if ((not defined $locus_id) or $locus_id eq '');
+
+			if (not exists $ortho_group->{$strain})
+			{
+				#print "No ortholog group name for $strain in set, skipping ...\n";
+			}
+			elsif (not exists $existence_hash{$strain})
+			{
+				$existence_hash{$strain} = 1;
+				push(@good_ortho_values, $group_en[$g]);
+			}
+			else
+			{
+				#print "Duplicate for strain $strain found, skipping ...\n";
+			}
+		}
+
+		if (scalar(@good_ortho_values) == $number_valid_strains_in_set)
+		{
+			push(@ortho_good_group, \@good_ortho_values);
+			$group_kept++;
+		}
+		else
+		{
+			#print STDERR "Skipping group as it does not contain a complete valid set of strains: $line\n";
+			$group_filtered++;
+		}
+		
+		$line = readline($g_h);
 	}
 
-	die "othomcl-input not defined\n".usage if (not defined $orthomcl_input);
-	die "orthomcl-input $orthomcl_input does not exist.\n".usage if (not -e $orthomcl_input);
+	close($g_h);
 
-	die "fasta-input not defined\n".usage if (not defined $fasta_input);
-	die "fasta-input $fasta_input not a valid directory\n".usage if (not -d $fasta_input);
+	return (\@ortho_good_group, $group_kept, $group_filtered);
+}
 
-	die "output not defined\n".usage if (not defined $output_dir);
+sub create_seq_gene_table
+{
+	my ($fasta_input_dir) = @_;
 
 	my %gene_seq; # hash table mapping sequence id to sequence for all input fasta files
-	opendir(my $dh, $fasta_input) or die "Could not open $fasta_input: $!";
+	opendir(my $dh, $fasta_input_dir) or die "Could not open $fasta_input_dir: $!";
 	my @files = grep {/\.fasta$/} readdir($dh);
 	foreach my $file (@files)
 	{
-		my $file_path = "$fasta_input/$file";
+		my $file_path = "$fasta_input_dir/$file";
 		my $in = new Bio::SeqIO(-file => "$file_path", -format => "fasta") or die "Could not open $file_path: $!";
 		my $seq = $in->next_seq;
 		while (defined $seq)
@@ -69,44 +138,75 @@ sub run
 	}
 	closedir($dh);
 
-	open(my $g_h, "<$orthomcl_input") or die "Could not open groups file $orthomcl_input: $!";
-	my $line = readline($g_h);
+	return \%gene_seq;
+}
 
-	mkdir $output_dir if (not -e $output_dir);
-	while(defined $line)
+sub run
+{
+	my ($orthomcl_input,$fasta_input,$output_dir, $strain_id);
+
+	if ( @_ && $_[0] eq __PACKAGE__)
 	{
-		my @group_en = split(/\s+/, $line);
-	
-		my ($group_name) = ($group_en[0] =~ /^([^:]*)/);
-		die "Error: invalid group name ".(defined $group_name ? $group_name : 'undefined') if (not defined $group_name);
-		my $filepath = "$output_dir/core.$group_name";
+		GetOptions('i|orthomcl-input=s' => \$orthomcl_input,
+		   'f|fasta-input=s' => \$fasta_input,
+		   'o|output-dir=s' => \$output_dir,
+		   'strain-id=s@' => \$strain_id) or die "Invalid options\n".usage;
+	}
+	else
+	{
+		($orthomcl_input, $fasta_input, $output_dir, $strain_id) = @_;
+	}
+
+	die "othomcl-input not defined\n".usage if (not defined $orthomcl_input);
+	die "orthomcl-input $orthomcl_input does not exist.\n".usage if (not -e $orthomcl_input);
+
+	die "fasta-input not defined\n".usage if (not defined $fasta_input);
+	die "fasta-input $fasta_input not a valid directory\n".usage if (not -d $fasta_input);
+
+	die "strain-id not defined, must define at least one".usage if (not defined $strain_id);
+	die "strain-id not defined, must define at least one".usage if (not (@$strain_id > 0));
+
+	die "output not defined\n".usage if (not defined $output_dir);
+
+	my ($ortho_group, $ortho_group_string) = create_set_table($strain_id); # hash table defining which strains we want to filter out for defining group of orthologs
+	print "Filtering $ortho_group_string using orthomcl file $orthomcl_input and fasta input directory $fasta_input\n";
+	my $seq_gene = create_seq_gene_table($fasta_input);
+
+	my ($groups, $group_kept, $group_filtered) = handle_ortho_groups($orthomcl_input,$ortho_group);
+	print "Filtered out $group_filtered of a total of ".($group_kept + $group_filtered)." groups\n";
+
+	my %strain_locus;
+	mkdir $output_dir if (not -e $output_dir);
+
+	for(my $i = 0; $i < @$groups; $i++)
+	{
+		my $ortho_group = $groups->[$i];
+
+		my $filepath = "$output_dir/core.$i";
 		open(my $fh, ">$filepath") or die "Could not write to $filepath: $!";
-	        for ( my $i = 1; $i < @group_en; $i++)
+	        for ( my $j = 0; $j < @$ortho_group; $j++)
 	        {
-	                my $en = $group_en[$i];
+			my $entry = $ortho_group->[$j];
 	
-	                if (not defined $en)
+	                if (not defined $entry)
 	                {
-	                        print STDERR "Warning: no proper id for line $line\n";
+	                        die "Error: undefined entry\n";
 	                }
 	                else
 	                {
-	                        my $seq = $gene_seq{$en};
+	                        my $seq = $seq_gene->{$entry};
 
 	                        if (not defined $seq)
 	                        {
-	                                print STDERR "Warning: no in sequence list for entry $en\n";
+	                                print STDERR "Warning: no in sequence list for entry $entry\n";
 	                        }
 	                        else
 	                        {
-	                                print $fh ">$en\n";
+	                                print $fh ">$entry\n";
                                		print $fh $seq->seq,"\n";
                         	}
 	                }
 		}
 		close($fh);
-
-		$line = readline($g_h);
         }
-	close($g_h);
 }
